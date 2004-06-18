6161240e836a99009f72752c1049e8fe93722c3d
vkdasari <vkdasari>
2004-06-18 20:27:49 +0000

    *** empty log message ***

---
+++ BookShop/src/com/bs/common/LogUtil.java
@@ -0,0 +1,19 @@
+/*
+ * Created on Jun 18, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package com.bs.common;
+
+/**
+ * @author Administrator
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+public class LogUtil {
+	public static void log(String msg){
+		System.out.println(msg);
+	}
+}
